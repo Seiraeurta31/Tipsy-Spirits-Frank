2885baac56eacf2d8d10ee71c140d048fd121e65
Ashley Frank <ashley.frank@ufl.edu>
2024-04-10 22:46:05 -0400

    Added outline for drink CRUD

---
+++ db/controllers/drink.js
@@ -0,0 +1,21 @@
+import User from './models/user'
+import dbConnect from '../connection'
+
+//CRUD for getting, creating, deleting drinks from "favorites"
+
+
+
+//TODO: READ/GET ALL drinks in favorites list
+
+
+
+//TODO: READ/GET single drink from favorites list by drink Id
+
+
+
+//TODO: CREATE/ADD drink to favorites list
+
+
+
+
+//TODO: DELETE/REMOVE book from Favorites list
