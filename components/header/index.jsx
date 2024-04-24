201f6b0a1a5f715a05e5d6e704d4dd9603780331
Ashley Frank <ashley.frank@ufl.edu>
2024-04-23 20:58:45 -0400

    trying to work on header

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -6,31 +6,48 @@ export default function Header(props) {
   const logout = useLogout();
   return (
     <header >
+
       {props.isLoggedIn ? (
         <>
+        <div classname={styles.navbar} >
+          <div classname={styles.img_container}>
+            <img></img>
+          </div>
+
+          <div classname={styles.navLinks_container}> 
+            <a href="/" classname={styles.link}>Home</a>
+            <a href="/favorites" classname={styles.link}>Favorites</a>
+            <a href="/search" classname={styles.link}>Search</a>
+            <a onClick={logout} classname={styles.link}>Logout</a>
+          </div>
+        </div>
+        
+
+          {/* <div >
+            <Link href="/search" >
+              <h2>Search for a drink</h2>
+            </Link>
+          </div>
           <p>
             <Link href="/">Home</Link>
           </p>
+          <p>Welcome, {props.username}!</p>
           <div >
-            <p>Welcome, {props.username}!</p>
-            <div >
-              <Link href="/search" >
-                <h2>Search for a drink</h2>
-              </Link>
-            </div>
-            <p onClick={logout} >
-              Logout
-            </p>
+            <Link href="/search" >
+              <h2>Search for a drink</h2>
+            </Link>
           </div>
+          <p onClick={logout} >
+            Logout
+          </p>
+          </div> */}    
         </>
       ) : (
         <>
-          <p>
-            <Link href="/">Home</Link>
-          </p>
-          <p>
-            <Link href="/login">Login</Link>
-          </p>
+          <div classname={styles.img_container}>
+            <img></img>
+          </div>
+          
         </>
       )}
     </header>
